703a4f9fbfbb30a19f051f41723536c8869202be
rizkyakbar1511 <rizkyakbar1511@gmail.com>
2025-02-03 16:52:10 +0700

    refactor: 💡 create oauth reusable button component

---
+++ src/components/oauth-signin.tsx
@@ -0,0 +1,31 @@
+import { signIn } from "@/auth";
+import Image from "next/image";
+import type { BuiltInProviderType } from "@auth/core/providers";
+import { cn } from "@/lib/utils";
+
+interface OAuthSignInProps {
+  provider: BuiltInProviderType;
+  iconPath: string;
+  className: string;
+}
+
+export default function OAuthSignIn({ provider, iconPath, className }: OAuthSignInProps) {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn(provider);
+      }}
+    >
+      <button
+        className={cn(
+          "flex items-center justify-center w-full gap-2 px-6 py-2 text-sm uppercase rounded-md hover:opacity-80",
+          className
+        )}
+      >
+        <Image width={24} height={24} src={iconPath} alt={provider} priority={true} />
+        Sign In with github
+      </button>
+    </form>
+  );
+}
