3657652ea6d53407a2e969b84cc94571466063ff
rizkyakbar1511 <rizkyakbar1511@gmail.com>
2025-02-15 19:43:17 +0700

    fix: 🐛 revert middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,30 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+import { PROTECTED_ROUTES, PUBLIC_ROUTES } from "./constants/routes";
+import { NextResponse } from "next/server";
+import { isAdmin } from "./lib/utils";
+
+export default auth((req) => {
+  const { pathname, origin } = req.nextUrl;
+  const isProtected = PROTECTED_ROUTES.some((route) => pathname.startsWith(route));
+  const isPublic = PUBLIC_ROUTES.some((route) => pathname.startsWith(route));
+
+  if (!req.auth && isProtected) {
+    const newUrl = new URL("/login", origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (req.auth && !isAdmin(req.auth.user.role) && pathname === "/dashboard") {
+    const newUrl = new URL("/", origin);
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (req.auth && isPublic) {
+    const redirectUrl = req.auth.user.role === "admin" ? "/dashboard" : "/";
+    const newUrl = new URL(redirectUrl, origin);
+    return NextResponse.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
